7af9b2280d202bbc9aa71dfd7dd69fbb84a39035
porotogus <gustavocarrena@outlook.com>
2022-08-13 19:09:25 -0300

    test level

--- src/components/TestLevel.jsx
+++ src/components/TestLevel.jsx
@@ -1,51 +1,55 @@
 import React from 'react';
+import {usePath} from "../hooks/usePath";
+import {Mydiv, Myimg, Mycontent, Mysection,Mycontainer, Mytestscontainer,Mytext, MyTitleContainer,MyArrowContainer} from '../templates/testLevelStyles';
 import styles from './testLevel.module.scss';
 
 export const TestLevel = () => {
-  return (
-    <section className={styles.sectionContainer}>
-        
-        <div className={styles.firstContainer}>
-            <div className={styles.leftImgContainer}>
-                <img className={styles.leftImg} src="assets/img/generales/iconos-izq.svg" alt="images" />
-                <img className={styles.rightImg}src="assets/img/generales/iconos-der.svg" alt="images" />
-            </div>
-            <div className={styles.allLevelsInfoContainer}>
-                <div className={styles.allLevels}>
-                    <h6 className={styles.allLevelsTitle}>ALL LEVELS (A1 to C2)</h6>
-                    <span className={styles.allLevelsLine}></span>
-                    <p className={styles.allLevelsText}>We base our learning programs on the Common European Framework of Reference for Languages to
-                        provide a solid learning structure to our courses.</p>
-                </div>
-                <button className={styles.allLevelsBtn} href="levels_information.html">Read More</button>
-            </div>
-            <div className={styles.rightImgContainer}>
-                <img className={styles.img} src="assets/img/generales/iconos-der.svg" alt="images"/>
-            </div>
-        </div>
+  
+    const screen = usePath();
 
-        <div className={styles.secondContainer}>
-            <div className={styles.testLevelsContainer}>
-                <img className={styles.testLevelsImg} src="assets/img/fotos/AdobeStock_103519056.jpg" alt="photo"/>
-                <div className={styles.testYourLevel}>
-                    <div className={styles.titleContainer}>
-                        <h2 className={styles.title}>Test your level</h2>
-                        <span className={styles.line}></span>
-                    </div>
-                    <p className={styles.testYourLevelText}>Help us offer you the course that best meets your needs. If you have prior
-                        knowledge, took some hours of Spanish classNamees in the past, or can conjugate irregular
-                        subjunctive verbs like a rockstar, take our leveling test.</p>
-                    <button className={styles.testYourLevelBtn} href="levels_information.html">Test Now!</button>
+
+
+  
+    return (
+        <Mysection pathlocation={screen} className={styles.sectionContainer}>
+            
+            <Mydiv pathlocation={screen} className={styles.firstContainer}>
+                <div className={styles.leftImgContainer}>
+                    <img className={styles.leftImg} src="assets/img/generales/iconos-izq.svg" alt="images" />
+                    <img className={styles.rightImg}src="assets/img/generales/iconos-der.svg" alt="images" />
                 </div>
-            </div>
-            <div className={styles.arrowContainer}>
-                <img className={styles.arrow} src="assets/img/hero/hero_flecha.svg" alt="arrow-image"/>
-            </div>
-        </div>
-        {/* <div className={styles.bottomBackgroundContainer}>
-            <img className={styles.bottomBackground} src="assets/img/generales/onda-04.svg" alt="background" />
-        </div> */}
-    
-    </section>
+                <div className={styles.allLevelsInfoContainer}>
+                    <div className={styles.allLevels}>
+                        <h6 className={styles.allLevelsTitle}>ALL LEVELS (A1 to C2)</h6>
+                        <span className={styles.allLevelsLine}></span>
+                        <p className={styles.allLevelsText}>We base our learning programs on the Common European Framework of Reference for Languages to
+                            provide a solid learning structure to our courses.</p>
+                    </div>
+                    <button className={styles.allLevelsBtn} href="levels_information.html">Read More</button>
+                </div>
+                <div className={styles.rightImgContainer}>
+                    <img className={styles.img} src="assets/img/generales/iconos-der.svg" alt="images"/>
+                </div>
+            </Mydiv>
+
+            <Mycontainer pathlocation={screen} className={styles.secondContainer}>
+                <Mytestscontainer pathlocation={screen} className={styles.testLevelsContainer}>
+                    <Myimg pathlocation={screen} className={styles.testLevelsImg} src="assets/img/fotos/AdobeStock_103519056.jpg" alt="photo"/>
+                    <Mycontent pathlocation={screen} className={styles.testYourLevel}>
+                        <MyTitleContainer pathlocation={screen} className={styles.titleContainer}>
+                            <img className={styles.titleImg} src="/assets/img/fotos/test.svg" alt="photo" />
+                        </MyTitleContainer>
+                        <Mytext pathlocation={screen} className={styles.testYourLevelText}>Help us offer you the course that best meets your needs. If you have prior
+                            knowledge, took some hours of Spanish classNamees in the past, or can conjugate irregular
+                            subjunctive verbs like a rockstar, take our leveling test.</Mytext>
+                        <button className={styles.testYourLevelBtn} href="levels_information.html">Test Now!</button>
+                    </Mycontent>
+                </Mytestscontainer>
+                <MyArrowContainer pathlocation={screen} className={styles.arrowContainer}>
+                    <img className={styles.arrow} src="assets/img/hero/hero_flecha.svg" alt="arrow-image"/>
+                </MyArrowContainer>
+            </Mycontainer>
+  
+    </Mysection>
   )
 }
---
+++ src/templates/testLevelStyles.js
@@ -0,0 +1,109 @@
+import styled from 'styled-components';
+
+
+const mydiv = styled.div`
+display:${props => (props.pathlocation === 'language' ? 'none !important' : '')}
+`
+
+const myimg = styled.img`
+display:${props => (props.pathlocation === 'language' ? 'none !important' : '')}
+`
+
+const mytestcontainer = styled.div`
+position:${props => (props.pathlocation === 'language' ? 'relative' : '')};
+top:${props => (props.pathlocation === 'language' ? '130px !important' : '')};
+
+@media(min-width:750px){
+    max-width:${props => (props.pathlocation === 'language' ? '230px !important' : '')};
+    position:${props => (props.pathlocation === 'language' ? 'relative' : '')};
+    top:${props => (props.pathlocation === 'language' ? '0px !important' : '')};
+    right:${props => (props.pathlocation === 'language' ? '-23%' : '')};
+}
+`
+
+const mycontent = styled.div`
+margin:${props => (props.pathlocation === 'language' ? '30px 0' : '')};
+@media(min-width:750px){
+    width:${props => (props.pathlocation === 'language' ? '100% !important' : '')};
+    margin:${props => (props.pathlocation === 'language' ? '166px 8% 32px 0px' : '')};
+    height:${props => (props.pathlocation === 'language' ? '295px !important' : '')};
+}
+@media(min-width:768px){
+    margin:${props => (props.pathlocation === 'language' ? '166px 8% 32px 0px' : '')};
+    height:${props => (props.pathlocation === 'language' ? '295px !important' : '')};
+}
+@media(min-width:1024px){
+    margin:${props => (props.pathlocation === 'language' ? '53px 35% 32px 0px' : '')};
+}
+`
+
+const myTextYourLevelText = styled.p`
+@media(min-width:768px){
+    font-size:${props => (props.pathlocation === 'language' ? '0.65rem !important' : '')};
+    margin-bottom:${props => (props.pathlocation === 'language' ? '15px !important' : '')};
+    width:${props => (props.pathlocation === 'language' ? '95% !important' : '')};
+}
+`
+
+const mysection = styled.section`
+background-color:${props => (props.pathlocation === 'language' ? 'transparent !important' : '')};
+`
+
+const mycontainer = styled.div`
+background-image: ${props => (props.pathlocation === 'language' ? 'url("/assets/img/fotos/levels.jpg") !important' : '')};
+background-repeat:${props => (props.pathlocation === 'language' ? 'no-repeat !important' : '')} ;
+background-position:${props => (props.pathlocation === 'language' ? 'center center !important' : '')} ;
+background-size:${props => (props.pathlocation === 'language' ? 'cover !important' : '')};
+width:${props => (props.pathlocation === 'language' ? '100vw !important' : '')};
+display: ${props => (props.pathlocation === 'language' ? 'flex !important' : '')};
+@media(min-width:1024px){
+    height:${props => (props.pathlocation === 'language' ? '681px !important' : '')};
+};
+`
+
+const mytitlecontainer = styled.div`
+@media(min-width:1024px){
+    widht:${props => (props.pathlocation === 'language' ? '260px !important' : '')};
+};
+`
+
+const myarrowcontainer = styled.div`
+@media(min-width:768px){
+    width:${props => (props.pathlocation === 'language' ? '86px !important' : '')};
+    margin-top:${props => (props.pathlocation === 'language' ? '209px !important' : '')};
+    right:${props => (props.pathlocation === 'language' ? '5% !important' : '')};
+};
+@media(min-width:820px){
+    right:${props => (props.pathlocation === 'language' ? '7% !important' : '')};
+}
+@media(min-width:960px){
+    right:${props => (props.pathlocation === 'language' ? '10% !important' : '')};
+}
+@media(min-width:1024px){
+    right:${props => (props.pathlocation === 'language' ? '12% !important' : '')};
+    margin-top:${props => (props.pathlocation === 'language' ? '56px !important' : '')};
+}
+@media(min-width:1024px){
+    right:${props => (props.pathlocation === 'language' ? '13% !important' : '')};
+}
+@media(min-width:1326px){
+    right:${props => (props.pathlocation === 'language' ? '15% !important' : '')};
+}
+@media(min-width:1440px){
+    right:${props => (props.pathlocation === 'language' ? '16% !important' : '')};
+}
+
+`
+
+
+export const Mydiv = mydiv;
+export const Myimg = myimg;
+export const Mycontent = mycontent;
+export const Mysection = mysection;
+export const Mycontainer = mycontainer;
+export const Mytestscontainer = mytestcontainer;
+export const Mytext = myTextYourLevelText;
+export const MyTitleContainer = mytitlecontainer;
+export const MyArrowContainer = myarrowcontainer;
+
+
